516bbee4d94390c262cdfaedcd262cb90ab4aa31
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-25 11:18:55 -0700

    Write basic logic for subscriptions destroy action

--- app/controllers/api/v0/customers/subscriptions_controller.rb
+++ app/controllers/api/v0/customers/subscriptions_controller.rb
@@ -13,6 +13,14 @@ class Api::V0::Customers::SubscriptionsController < ApplicationController
   end
 
   def destroy
+    subscription = Subscription.find(params[:id])
+
+    if subscription.status == 1
+      subscription.status = 2
+      render json: SubscriptionSerializer.new(subscription), status: :ok
+    else
+      render json: { errors: 'subscription is already cancelled' }, status: :unprocessable_entity
+    end
   end
 
   def index
